48980fabe55062168c077bee8ac26006131c8157
Serena Chen <serenazychen@gmail.com>
2016-03-02 16:21:06 -0500

    initial files

---
+++ Display.py
@@ -0,0 +1,9 @@
+
+class Screen(object):
+	"""has a screen, takes in models to draw, keyboard control, if applicable"""
+
+class PieGraph(object):
+	"""Stores the data for the graph to display (as a dictionary?), creates the model
+		for drawing arcs"""
+class EventController(object):
+	"""handles keyboard and mouse input, using the handle_event method"""
