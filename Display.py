ab3f665775393975725ce0979601ef55b7947be8
cm898946 <cm898946@gmail.com>
2016-03-02 18:05:58 -0500

    Now draws the pie charts; 2 doctest failures

--- Display.py
+++ Display.py
@@ -10,17 +10,18 @@ class Screen(object):
 	def __init__(self, model, size):
 		self.model = model
 		self.screen = pygame.display.set_mode(size)
-		self.base_rect = (500,400,300,300)
+		self.base_rect = (100,100,800,800)
 
 	def draw(self):
-		self.screen.fill(pygame.Color('black'))
+		self.screen.fill(pygame.Color('white'))
 		for arc in self.model.get_arcs():
 			pygame.draw.arc(
 				self.screen,
 				arc['color'],
 				self.base_rect, 
 				arc['start_angle'],
-				arc['stop_angle'])
+				arc['stop_angle'],
+				400)
 		pygame.display.update()
 
 class PieGraph(object):
@@ -65,7 +66,8 @@ class PieGraph(object):
 		perc = {}
 		for k in self.data.keys():
 			perc[k]=(float(self.data[k])/self.raw_total)
-		return sorted(perc.items(),key=lambda x:x[1],reverse=True)
+		return sorted(perc.items(),key=lambda x:x[1],)
+
 	def update_arcs(self):
 		"""makes a new list of dictionaries based on the current data in the PieGraph.
 			Stores a list of dictionaries.
@@ -74,12 +76,13 @@ class PieGraph(object):
 		curr_angle = pi/2
 		for t in self.calculate_percent():
 			d = {}
-			d['color'] = (randrange(256),randrange(256),randrange(256))
+			d['color'] = (randrange(256),randrange(256),randrange(256), 255)
 			d['label'] = t[0]
 			d['start_angle']=curr_angle
-			d['end_angle']=curr_angle-(2*pi*t[1])
-			curr_angle = d['end_angle']
+			d['stop_angle']=curr_angle+(2*pi*t[1])
+			curr_angle = d['stop_angle']
 			self.arcs.append(d)
+			self.arcs[-1]['stop_angle']=5*pi/2+.04
 
 	def get_arcs(self):
 		"""Returns a list of dictionaries.
@@ -109,9 +112,9 @@ if __name__ == '__main__':
 	print pg.get_arcs()
 
 	pygame.init()
-	size = (1000, 800)
+	size = (1000, 1000)
 
-	model = PieGraph()
+	model = pg
 	view = Screen(model, size)
 	controller = EventController(model)
 	running = True
@@ -121,4 +124,5 @@ if __name__ == '__main__':
 				running = False
 			#else:
 			#	EventController.handle_event()
+		view.draw()
 		time.sleep(float(1/60))
